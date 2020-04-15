92a8f3f9b20477b8433b9a2ad45a038c3fa7afb6
roanurquhart <roanurquhart@gmail.com>
2020-04-15 13:29:33 -0400

    model_based unfinished, untested

--- utility.py
+++ utility.py
@@ -1,11 +1,14 @@
 states = {}
 actions = []
+rewards = {'Fairway': 1, 'Ravine': 2, 'Close': 1, 'Same': 1, 'Left': 1, 'Over': 1, 'In': 0}
 
 
 class State:
     def __init__(self, name):
         self.name = name
         self.avail_actions = {}
+        self.transition_track = {}
+        self.reward_track = {}
 
     def __repr__(self):
         return "<State: %s>" % self.name
@@ -30,8 +33,11 @@ def parse_input(data):
         if segmented[1] not in state.avail_actions:
             # Nested dictionary - {Action: {newState: probability}}
             state.avail_actions[segmented[1]] = {}
+            state.transition_track[segmented[1]] = {}
+            state.reward_track[segmented[1]] = 0
 
         state.avail_actions[segmented[1]][segmented[2]] = float(segmented[3].rstrip())
+        state.transition_track[segmented[1]][segmented[2]] = 0
 
         counter += 1
 
